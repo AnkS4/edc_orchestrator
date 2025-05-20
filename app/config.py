84e540d487736ee0e2eb90298c7feb69c446d018
Aniket Bharatrao Satbhai <aniket.bharatrao.satbhai@upc.edu>
2025-05-20 10:55:48 +0200

    Increase default request timeout from 3 to 5 seconds in config

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     """Application configuration settings."""
     # SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-for-dev-only')
-    REQUEST_TIMEOUT = int(os.environ.get('REQUEST_TIMEOUT', 3))
+    REQUEST_TIMEOUT = int(os.environ.get('REQUEST_TIMEOUT', 5))
     # MAX_RETRIES = int(os.environ.get('MAX_RETRIES', 2))
     DATA_ADDRESS_DELAY = int(os.environ.get('DATA_ADDRESS_DELAY', 1))
     DATA_ADDRESS_MAX_RETRIES = int(os.environ.get('DATA_ADDRESS_MAX_RETRIES', 3))
